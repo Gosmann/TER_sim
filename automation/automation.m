9134de85920c83990801a23df73ad40c1b7743a4
Felipe-noob <felipelorenzzon@gmail.com>
2024-02-01 16:56:21 +0100

    graphical improvements

--- automation/automation.m
+++ automation/automation.m
@@ -23,8 +23,8 @@ freq_i = 2;
 % have a secondary file only with a machine and a charge in an isolated
 % grid.
 
-calibration = 0;
-if calibration == 1
+calibrationEnable = 0;
+if calibrationEnable == 1
     "Machine 1"                
     % first pass
     open(CalibrationModel);
@@ -110,22 +110,9 @@ for ren_percent = 0:0.1:0.3
     freq = simOut.logsout{6};
     % after closing
     freq_2_array = freq.Values.Data(close_time/0.0002 - 500 + 2: open_time/0.0002 -500);
-    % after reopening
-    freq_3_array = freq.Values.Data(open_time/0.0002 - 500 + 2: end);
 
     % VOLTAGE
     tension = simOut.logsout{1};
-
-    % P_ref_1 e 2
-    % The control signal after the droop that sets the torque (pu)
-    P_ref_1 = simOut.logsout{11};
-    P_ref_2 = simOut.logsout{12};
-    % after closing
-    P_ref_1_2_array = P_ref_1.Values.Data(close_time/0.0002 - 500 + 2: open_time/0.0002 -500);
-    P_ref_2_2_array = P_ref_2.Values.Data(close_time/0.0002 - 500 + 2: open_time/0.0002 -500);
-    % after reopening
-    P_ref_1_3_array = P_ref_1.Values.Data(open_time/0.0002 - 500 + 2: end);
-    P_ref_2_3_array = P_ref_2.Values.Data(open_time/0.0002 - 500 + 2: end);
 
     % POWER
     P = simOut.logsout{2};
@@ -134,73 +121,14 @@ for ren_percent = 0:0.1:0.3
     P_sol_array = simOut.logsout{5};
     P_eol_array = simOut.logsout{9};
 
-    % after closing
-    P_2_array = P.Values.Data(close_time/0.0002 - 500 + 2: open_time/0.0002 -500);    
-    P1_2_array = P1.Values.Data(close_time/0.0002 - 500 + 2: open_time/0.0002 -500);
-    P2_2_array = P2.Values.Data(close_time/0.0002 - 500 + 2: open_time/0.0002 -500);
-
-
-    % after reopening
-    P_3_array = P.Values.Data(open_time/0.0002 - 500 + 2: end);
-    P1_3_array = P1.Values.Data(open_time/0.0002 - 500 + 2: end);    
-    P2_3_array = P2.Values.Data(open_time/0.0002 - 500 + 2: end);
-
     freq_1 = freq.Values.Data(close_time/0.0002 - 500 );
     freq_2 = freq_2_array(end);
     delta_f = freq_2 - freq_1;
     delta_f_array(i) = abs(delta_f);
 
-    % SAVING THE RESULTS
-    resultArray(i).freq_1 = freq_1;
-    resultArray(i).freq_2 = freq_2;
-    resultArray(i).freq_3 = freq_3_array(end);
-
-    resultArray(i).freq_2_min = min(freq_2_array);
-    resultArray(i).freq_3_max = max(freq_3_array);
-
-    resultArray(i).tension_1 = tension.Values.Data(close_time/0.0002 - 500 );
-    resultArray(i).tension_2 = tension.Values.Data(open_time/0.0002 -500);
-    resultArray(i).tension_3 = tension.Values.Data(end);
-
-    resultArray(i).P_ref_1_1 = P_ref_1.Values.Data(close_time/0.0002 - 500 );
-    resultArray(i).P_ref_2_1 = P_ref_2.Values.Data(close_time/0.0002 - 500 );
-    resultArray(i).P_ref_1_2 = P_ref_1_2_array(end);
-    resultArray(i).P_ref_2_2 = P_ref_2_2_array(end);
-    resultArray(i).P_ref_1_3 = P_ref_1_3_array(end);
-    resultArray(i).P_ref_2_3 = P_ref_2_3_array(end);
-
-    resultArray(i).P_ref_1_2_max = max(P_ref_1_2_array);
-    resultArray(i).P_ref_2_2_max = max(P_ref_2_2_array);
-    resultArray(i).P_ref_1_3_min = min(P_ref_1_3_array);
-    resultArray(i).P_ref_2_3_min = min(P_ref_2_3_array);
-
-    resultArray(i).P_1 = P.Values.Data(close_time/0.0002 - 500 );
-    resultArray(i).P1_1 = P1.Values.Data(close_time/0.0002 - 500 );
-    resultArray(i).P2_1 = P2.Values.Data(close_time/0.0002 - 500 );
-    resultArray(i).P_2 = P_2_array(end);
-    resultArray(i).P1_2 = P1_2_array(end);
-    resultArray(i).P2_2 = P2_2_array(end);
-    resultArray(i).P_3 = P_3_array(end);
-    resultArray(i).P1_3 = P1_3_array(end);
-    resultArray(i).P2_3 = P2_3_array(end);
-
-    resultArray(i).P_2_min = min(P_2_array);
-    resultArray(i).P_2_max = max(P_2_array);
-    resultArray(i).P1_2_min = min(P1_2_array);
-    resultArray(i).P1_2_max = max(P1_2_array);
-    resultArray(i).P2_2_min = min(P2_2_array);
-    resultArray(i).P2_2_max = max(P2_2_array);
-    resultArray(i).P_3_max = max(P_3_array);
-    resultArray(i).P1_3_max = max(P1_3_array);
-    resultArray(i).P2_3_max = max(P2_3_array);
-
-    resultArray(i).P_machines = P_M; 
-    resultArray(i).P_eol = P_eol;
-    resultArray(i).P_PV = 0;
-    resultArray(i).P_charge = Pn_L1;
-    resultArray(1).ren_percent = ren_percent;
-
-    plotSim(freq, P, P1, P2, tension, P_ref_1, P_ref_2, ren_percent, P_sol_array, P_eol_array);
+    P1_pu_array = simOut.logsout{11};
+    P2_pu_array = simOut.logsout{13};
+    plotSim(freq, P1_pu_array, P2_pu_array, P, P1, P2, tension, ren_percent, P_sol_array, P_eol_array);
 end
 
 figure(5)
@@ -209,7 +137,6 @@ plot(percantageArray, delta_f_array, 'DisplayName', "delta_f");
 title("Frequency deviation in terms of the renewable percentage (pu)");
 legend
 
-save("results.mat", "resultArray");
 msgbox("Done");
 
 function plotM_1(P_ref, freq, machine)
@@ -241,50 +168,55 @@ function plotM_2(P_ref, freq, machine)
     legend
 end
 
-function plotSim(freq, P, P1, P2, tension, P_ref_1, P_ref_2, r, P_sol_array, P_eol_array)
+function plotSim(freq, P1_pu_array, P2_pu_array, P, P1, P2, tension, r, P_sol_array, P_eol_array)
     % Graphs for debugging the simulation
     figure(3)
     set(gcf,'name','Simulation');
     subplot(2,2,1);
-    plot(freq.Values.Time, freq.Values.Data, 'DisplayName', freq.Name);
+    plot(freq.Values.Time, freq.Values.Data, 'DisplayName', freq.Name, 'LineWidth', 3);
     xlim([10 40])
+    xlabel('t [s]')
     title("Frequency (pu)");
     legend
     
     subplot(2,2,2);
-    plot(P.Values.Time, P.Values.Data, 'DisplayName', P.Name);
+    plot(P.Values.Time, P.Values.Data, 'DisplayName', P.Name, 'LineWidth', 3);
     hold on
-    plot(P1.Values.Time, P1.Values.Data, 'DisplayName', P1.Name);
-    plot(P2.Values.Time, P2.Values.Data, 'DisplayName', P2.Name);
-    plot(P_sol_array.Values.Time, P_sol_array.Values.Data, 'DisplayName', P_sol_array.Name);
-    plot(P_eol_array.Values.Time, P_eol_array.Values.Data, 'DisplayName', P_eol_array.Name);
+    plot(P1.Values.Time, P1.Values.Data, 'DisplayName', P1.Name, 'LineWidth', 3);
+    plot(P2.Values.Time, P2.Values.Data, 'DisplayName', P2.Name, 'LineWidth', 3);
+    plot(P_sol_array.Values.Time, P_sol_array.Values.Data, 'DisplayName', P_sol_array.Name, 'LineWidth', 3);
+    plot(P_eol_array.Values.Time, P_eol_array.Values.Data, 'DisplayName', P_eol_array.Name, 'LineWidth', 3);
     title("Power measurements (W)");
     xlim([10 40])
+    xlabel('t [s]')
     hold off
     legend
     
     subplot(2,2,3);
-    plot(tension.Values.Time, tension.Values.Data, 'DisplayName', tension.Name);
+    plot(tension.Values.Time, tension.Values.Data, 'DisplayName', tension.Name, 'LineWidth', 3);
     title("Tension measurements (pu)");
     xlim([10 40])
+    xlabel('t [s]')
     legend
     
     subplot(2,2,4);
-    plot(P_ref_1.Values.Time, P_ref_1.Values.Data, 'DisplayName', P_ref_1.Name);
+    plot(P1_pu_array.Values.Time, P1_pu_array.Values.Data, 'DisplayName', P1_pu_array.Name, 'LineWidth', 3);
     hold on
-    plot(P_ref_2.Values.Time, P_ref_2.Values.Data, 'DisplayName', P_ref_2.Name);
-    title("Power command (pu)");
+    plot(P2_pu_array.Values.Time, P2_pu_array.Values.Data, 'DisplayName', P2_pu_array.Name, 'LineWidth', 3);
+    title("Power command and measures (pu)");
     xlim([10 40])
+    xlabel('t [s]')
     hold off
     legend
     
     %superposition of frequency graphs
     figure(4)
     set(gcf,'name','Frequency supeposition');
-    plot(freq.Values.Time, freq.Values.Data, 'DisplayName', freq.Name + " (" + string(r) + ")");
+    plot(freq.Values.Time, freq.Values.Data, 'DisplayName', freq.Name + " (" + string(r) + ")", 'LineWidth', 3);
     hold on
     title("Instant frequency for different intermittent percentages (pu)");
     xlim([10 40])
+    xlabel('t [s]')
     legend
 
     % backuping in a file
